ea2790862e6f331853fff677d45635ef729d2eb1
retguard <160794657+retguard@users.noreply.github.com>
2024-02-21 16:33:14 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+main() {
+    echo -e "Downloading Latest Roblox"
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    curl "https://setup.rbxcdn.com/mac/version-10ec0daef5cd4006-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+
+    echo -e "Installing Latest Roblox"
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+
+    echo -e "Downloading libHydrogen"
+    rm ./libHydrogen.dylib
+    curl -LJO "https://github.com/retguard/legendary-couscous/raw/main/libHydrogen.dylib"
+
+    rm ./insert_dylib
+    echo -e "Downloading insert_dylib"
+    curl -LJO "https://github.com/retguard/legendary-couscous/raw/main/insert_dylib"
+
+    chmod +x "./insert_dylib"
+
+    echo -e "Patching Roblox"
+    mv ./libHydrogen.dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib"
+    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+
+    chmod +x "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib"
+
+    echo -e "Install Complete!"
+}
+
+main
